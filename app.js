28ced6eeeb9e7202b61ecd1f0e5478935eff812e
Emmanuel Tweneboah Koduah <emmanueltweneboah71@gmail.com>
2022-05-26 09:55:50 +0000

    installed morgan and implemented it to show me dtails of each request made to this api

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const productRoute = require("./api/routes/productRoute");
+const morgan = require("morgan")
 // const ordertRoute = require("./api/routes/orderRoute");
 const app = express();
 
+app.use(morgan("dev"));
 app.use("/products" ,productRoute);
 // app.use("/orders" ,orderRoute);
 
